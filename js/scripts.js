863cdef53d083384c69450caa0f06071b5878c52
Sharanc25 <Sharanc25@gmail.com>
2015-02-26 15:39:21 +0530

    Adds Menu Items

--- js/scripts.js
+++ js/scripts.js
@@ -71,16 +71,16 @@ $(".nav-toggle").click(function() {
 });
 
 /* When user clicks a link */
-$(".overlay ul li a").click(function() {
+/*$(".overlay ul li a").click(function() {
 	$(".nav-toggle").toggleClass("active");
 	$(".overlay-boxify").toggleClass("open");
-});
+}); */
 
 /* When user clicks outside */
-$(".overlay").click(function() {
+/* $(".overlay").click(function() {
 	$(".nav-toggle").toggleClass("active");
 	$(".overlay-boxify").toggleClass("open");
-});
+}); */
 
 /***************** Smooth Scrolling ******************/
 
