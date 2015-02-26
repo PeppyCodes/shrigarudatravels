5215daef42d65c83d2c287d11e9a8ce9096626b8
Sharanc25 <Sharanc25@gmail.com>
2015-02-26 17:19:08 +0530

    Modifies Menu bar

--- js/scripts.js
+++ js/scripts.js
@@ -65,9 +65,8 @@ $(document).ready(function() {
 /***************** Nav Transformicon ******************/
 
 /* When user clicks the Icon */
-$(".nav-toggle").click(function() {
-	$(this).toggleClass("active");
-	$(".overlay-boxify").toggleClass("open");
+$(".overlay ul li").hover(function() {
+	$(this).find("i").toggleClass("show");
 });
 
 /* When user clicks a link */
